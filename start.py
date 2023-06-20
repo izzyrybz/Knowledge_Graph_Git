8271c62a6bbe375658f13d4b78825acab1203c42
izzyrybz <70098404+izzyrybz@users.noreply.github.com>
2023-06-20 14:10:25 +0200

    Create start.py

---
+++ start.py
@@ -0,0 +1,34 @@
+import subprocess
+import os
+
+from process_logfile import process_logfile
+
+'''import sys
+sys.path.append('/home/bell/rdf_code/ownbuild.py')
+import ownbuild'''
+
+print("#"*80)
+print("Welcome to the knowledge graph generator")
+print("#"*80)
+
+#possible option in the future to let user enter the attributes that they want to look at and then extend the process_logfile
+#attri = input("Please provide the attributes investigate, such as commit hash, user, date, ect \n(read more on : https://git-scm.com/docs/git-log) \n")
+#attri = '%ncommit:%H,Author:%an,Description: %s,Date:%cd,Parents:%p%nChanged Files:%n'
+#process = subprocess.Popen(['./git_fetch_script.sh', repo, attri], stdout=subprocess.PIPE)
+
+repo = input("Please provide the path that the repo is saved under, from the file you are in right now \n(Example: ../rdf_code) \n")
+
+process = subprocess.Popen(['./git_fetch_script.sh', repo], stdout=subprocess.PIPE)
+
+output, error = process.communicate()
+print(output.decode())
+
+if (os.path.dirname(os.path.realpath('logfile2.txt'))):
+
+    ttl_file_path = process_logfile('logfile2.txt')
+
+print("we are done")
+
+
+
+
